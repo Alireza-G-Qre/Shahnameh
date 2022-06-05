e44ea48c8402750bda52c3492c422cb36f7c968c
Pouya Esmaili <poesmaili@gmail.com>
2022-06-05 16:09:22 +0430

    Update poems

---
+++ datasets/extract.py
@@ -0,0 +1,18 @@
+import io
+from bs4 import BeautifulSoup
+
+with io.open('./datasets/shahname-ferdosi.htm', 'r', encoding='utf-8') as file:
+    html = file.read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+
+def filter_poems(tag):
+    return tag.name == 'span' and tag.has_attr('class') and 'content_text' in tag.get('class') and '****' in tag.get_text()
+
+
+poems = soup.find_all(filter_poems)
+
+with io.open('./datasets/shahname.txt', 'w', encoding='utf-8') as file:
+    for item in poems:
+        file.writelines(map(lambda x: f'{x}\n', item.get_text().split('****')))
