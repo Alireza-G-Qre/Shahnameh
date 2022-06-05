c9fb12d4d748dd1a2340e6324d906a916ea594a7
Ubuntu <esmaili.1380@gmail.com>
2022-06-05 16:47:24 +0000

    Update

--- datasets/extract.py
+++ datasets/extract.py
@@ -1,7 +1,7 @@
 import io
 from bs4 import BeautifulSoup
 
-with io.open('./datasets/shahname-ferdosi.htm', 'r', encoding='utf-8') as file:
+with io.open('./datasets/shahnameh-ferdosi.htm', 'r', encoding='utf-8') as file:
     html = file.read()
 
 soup = BeautifulSoup(html, 'html.parser')
@@ -13,6 +13,6 @@ def filter_poems(tag):
 
 poems = soup.find_all(filter_poems)
 
-with io.open('./datasets/shahname.txt', 'w', encoding='utf-8') as file:
+with io.open('./datasets/shahnameh.txt', 'w', encoding='utf-8') as file:
     for item in poems:
         file.writelines(map(lambda x: f'{x}\n', item.get_text().split('****')))
